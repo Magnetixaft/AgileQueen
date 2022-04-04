293b720d302d8f18464fb2dd3523d5edf3b319df
Johan Andersson <1994.johan.andersson@gmail.com>
2022-04-04 14:13:07 +0200

    Added my name

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text('Hello World, Johan'),
         ),
       ),
     );
