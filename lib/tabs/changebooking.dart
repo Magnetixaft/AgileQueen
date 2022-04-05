748941a3d5425e950b582de77f4e6808e7f87b27
Andreas Helgesson <andreas.helgesson@hotmail.com>
2022-04-05 17:11:14 +0200

    Refactored code and added tabs

---
+++ lib/tabs/changebooking.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChangeBookingView extends StatelessWidget {
+  const ChangeBookingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Center(
+      child: Text("Hello World! Here we should change bookings."),
+    );
+  }
+}
---
+++ lib/tabs/createbooking.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreateBookingView extends StatelessWidget {
+  const CreateBookingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Center(
+      child: Text("Hello World! Here we should create bookings."),
+    );
+  }
+}
