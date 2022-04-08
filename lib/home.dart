841777fa9135b8bf7b276a3883664ea4ce1d746f
Andreas Helgesson <andreas.helgesson@hotmail.com>
2022-04-08 21:18:22 +0200

    Created Android interface and enabled compatability with android emulator

---
+++ lib/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/colors.dart';
+import 'package:flutter_application_1/tabs/bookingsview.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text("Index 0: Book"), // TODO Replace with tab
+    Text("Index 1: Bookings"), // TODO Replace with tab
+    Text("Index 2: Menu"), // TODO Replace with tab
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: elicitWhite,
+      appBar: AppBar(
+        backgroundColor: elicitWhite,
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(10,0,0,0),
+          child: Image.asset('assets/images/elicit_logo.png'),
+        ),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Book',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'My Bookings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: 'Menu',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: elicitGreen,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,26 @@ import 'package:flutter_application_1/FirebaseHandler.dart';
 import 'package:flutter_application_1/tabs/booking_view.dart';
 
 import 'package:flutter_application_1/tabs/changebooking.dart';
+import 'package:flutter_application_1/home.dart';
 import 'package:flutter_application_1/tabs/current_booking_view.dart';
 import 'package:flutter_application_1/tabs/firebasedemoview.dart';
-import 'package:flutter_application_1/tabs/login_screen.dart';
 
 import 'colors.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // Firebase options related to FirebaseDB
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyD71VJDiqwq5e2y7gpaszs4um91jR6tN1g",
-        authDomain: "agilequeen-82096.firebaseapp.com",
-        projectId: "agilequeen-82096",
-        storageBucket: "agilequeen-82096.appspot.com",
-        messagingSenderId: "883336254219",
-        appId: "1:883336254219:web:7d2de78527260bb27e080e")
-    );
+      // Firebase options related to FirebaseDB
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyD71VJDiqwq5e2y7gpaszs4um91jR6tN1g",
+          authDomain: "agilequeen-82096.firebaseapp.com",
+          projectId: "agilequeen-82096",
+          storageBucket: "agilequeen-82096.appspot.com",
+          messagingSenderId: "883336254219",
+          appId: "1:883336254219:web:7d2de78527260bb27e080e"));
 
   runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
@@ -37,18 +33,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Room Bookings',
       theme: ThemeData(
-        primarySwatch: Colors.blue, // TODO Custom Swatch
+        primarySwatch: Colors.grey, // TODO Custom Swatch
       ),
       home: FutureBuilder(
         //Initializes Firebase
         future: Firebase.initializeApp(),
         builder: (context, snapshot) {
           //If Connection with Firebase failed
-          if(snapshot.hasError){
+          if (snapshot.hasError) {
             print("Firebase initialization error");
           }
           //Checks connection to Firebase and when done loads HomePage
-          if(snapshot.connectionState == ConnectionState.done){
+          if (snapshot.connectionState == ConnectionState.done) {
             print("Firebase initialized correctly");
             return const MyHomePage(
               title: "Room Bookings",
@@ -70,117 +66,80 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
-class _MyHomePageState extends State<MyHomePage> implements LoginListener{
-  int _selectedIndex = 0;
-  List<Widget> _buildTabViews = <Widget>[];
-
-  _MyHomePageState() {
-    updateState();
-  }
-
-  void updateState() {
-    if (!FirebaseHandler.isInitialized()) {
-      _buildTabViews = [
-        LoginView(this),
-        const ChangeBookingView(),
-      ];
-    }
-    else {
-      _buildTabViews = [
-        const BookingView(),
-        const CurrentBookingView(),
-        FirebaseDemoView(),
-        LoginView(this),
-      ];
-    }
-  }
-
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: <Widget>[
-          NavigationRail(
-            selectedIndex: _selectedIndex,
-            onDestinationSelected: (int index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            backgroundColor: elicitGreen,
-            groupAlignment: 0.0,
-            labelType: NavigationRailLabelType.selected,
-            destinations: getNavigatorRailItems()
-          ),
-          const VerticalDivider(thickness: 1, width: 1),
-          // This is the main content.
-          Expanded(
-            child: Center(
-              child: _buildTabViews[_selectedIndex],
+        backgroundColor: elicitWhite,
+        resizeToAvoidBottomInset: false,
+        body: Column(
+          children: [
+            Expanded(
+              child: Center(
+                  child: Padding(
+                padding: const EdgeInsets.fromLTRB(40, 80, 40, 20),
+                child: Image.asset('assets/images/elicit_logo.png'),
+              )),
             ),
-          )
-        ],
-      ),
-    );
-  }
-
-  List<NavigationRailDestination> getNavigatorRailItems() {
-
-    if (FirebaseHandler.isInitialized()) {
-      return <NavigationRailDestination>[
-        NavigationRailDestination(
-          icon: Icon(Icons.calendar_month_outlined, color: elicitWhite, size: 40,),
-          selectedIcon: Icon(Icons.calendar_month, color: elicitWhite, size: 40,),
-          label: Text('Create Bookings', style: TextStyle(color: elicitWhite),),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.save_outlined, color: elicitWhite, size: 40,),
-          selectedIcon: Icon(Icons.create, color: elicitWhite, size: 40,),
-          label: Text('View Booking', style: TextStyle(color: elicitWhite),),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.fire_extinguisher_outlined, color: elicitWhite, size: 40,),
-          selectedIcon: Icon(Icons.fire_extinguisher, color: elicitWhite, size: 40,),
-          label: Text('Firebase Demo', style: TextStyle(color: elicitWhite),),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.lock_open_outlined, color: elicitWhite, size: 40,),
-          selectedIcon: Icon(Icons.cancel, color: elicitWhite, size: 40,),
-          label: Text('Change User', style: TextStyle(color: elicitWhite),),
-        ),
-      ];
-    }
-    else {
-      return [
-        NavigationRailDestination(
-          icon: Icon(Icons.lock_open_outlined, color: elicitWhite, size: 40,),
-          selectedIcon: Icon(Icons.cancel, color: elicitWhite, size: 40,),
-          label: Text('Login', style: TextStyle(color: elicitWhite),),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.lock_outline, color: elicitWhite, size: 40,),
-          selectedIcon: Icon(Icons.cancel, color: elicitWhite, size: 40,),
-          label: Text('Unavailable', style: TextStyle(color: elicitWhite),),
-        ),];
-    }
-  }
-
-
-  @override
-  void alert() {
-    updateState();
-    setState(() {
-      _selectedIndex = 0;
-    });
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Email or phone number',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Password',
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () => {
+                          print("Todo, Show modal"),
+                        },
+                        child: const Text("Forgot Password?"),
+                      ),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  elicitGreen)),
+                          onPressed: () => {
+                                print("TODO Login using Firebase Auth"), // TODO Add text controllers for above fields an loding using firebase auth
+                                Navigator.push(
+                                    // TODO use pushReplacement when login has been implemented. That way, the uswer will not be able to return to this page by pressing the back arrow
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            Home()))
+                              },
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: elicitWhite),
+                          )),
+                    ),
+                    TextButton(
+                      onPressed: () => {
+                        print("Todo, Show modal"),
+                      },
+                      child: const Text("Create an account"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
-
-abstract class LoginListener {
-  void alert();
-}
-
-
-
-
