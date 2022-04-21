62e1c25de032e78be95e19e6a4af0fdfc7fc6f5f
gujoh <gustafjohanson@live.com>
2022-04-21 12:42:26 +0200

    Made the 'my bookings' tab a lot nicer

--- lib/tabs/bookings.dart
+++ lib/tabs/bookings.dart
@@ -10,15 +10,42 @@ class Bookings extends StatefulWidget {
 }
 
 class _BookingsState extends State<Bookings> {
-  List<BookingItem> bookingList = <BookingItem>[];
+  List<Widget> bookingList = <Widget>[];
+
   @override
   Widget build(BuildContext context) {
     for (var i = 0; i < 1000; i++) {
-      bookingList.add(const BookingItem());
+      if (i % 13 == 0) {
+        bookingList.add(const SizedBox(height: 10));
+        bookingList.add(const Text(
+          "Today",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold),
+          ), 
+        );
+        bookingList.add(const SizedBox(height: 5));
+      } else {
+        bookingList.add(const BookingItem());
+      }
     }
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: bookingList,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const SizedBox(height: 10),
+        const Text("    My bookings",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            )),
+        const Divider(),
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: bookingList,
+          ),
+        ),
+      ],
     );
   }
 }
