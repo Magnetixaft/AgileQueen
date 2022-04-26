c222959129dca314d4b137a25b92e9cd14c00d81
albertwickman <albert.wickman@hotmail.se>
2022-04-26 09:13:33 +0200

    Profile Page is completed

--- lib/tabs/profile_settings.dart
+++ lib/tabs/profile_settings.dart
@@ -16,6 +16,7 @@ class Profile extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 20),
+
         Padding(
           padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
           child: Container(
@@ -27,23 +28,95 @@ class Profile extends StatelessWidget {
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [
+              children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(16),
                   child: CircleAvatar(
                     backgroundColor: Colors.white,
                     radius: 30,
                     child: ClipRRect(
-                      child: Image.network('https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'),
+                      child: Image.network('https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'), // temporary image
                       borderRadius: BorderRadius.circular(50.0),
                     ),
                   ),
                 ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      'Albert Wickman',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        letterSpacing: 1
+                      ),
+                    ),
+                    Text(
+                      'albert.wickman@hotmail.se',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
-        )
+        ),
+        buildSettingItem(context, 'My Account', 'Make changes to my profile'),
+        const Divider(height: 0, indent: 20, endIndent: 20),
+
+        buildSettingItem(context, 'Notifications', 'Adjust notificaton settings'),
+        const Divider(height: 0, indent: 20, endIndent: 20),
+
+        buildSettingItem(context, 'Help', 'Read FAQ'),
+        const Divider(height: 0, indent: 20, endIndent: 20),
+
+        buildSettingItem(context, 'About Elli', 'Read more about Elli'),
+        const Divider(height: 0, indent: 20, endIndent: 20),
+
+        buildSettingItem(context, 'Log Out', 'Leave the application'),
+        const Divider(height: 0, indent: 20, endIndent: 20),
       ],
+    );
+
+  }
+
+  GestureDetector buildSettingItem(BuildContext context, String title, String subtitle) {
+    return GestureDetector(
+      onTap: () {
+
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title),
+                  const SizedBox(height: 8),
+                  Text(
+                      subtitle,
+                      style: const TextStyle(
+                        color: Colors.grey
+                  )),
+                ],
+              ),
+            ),
+            const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.grey
+            )
+          ],
+        ),
+      ),
     );
   }
 }
