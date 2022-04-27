6bc927eee21004327783157404280827979f4018
albertwickman <albert.wickman@hotmail.se>
2022-04-27 12:50:23 +0200

    Added dark mode toggle

--- lib/tabs/profile_settings.dart
+++ lib/tabs/profile_settings.dart
@@ -1,7 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  bool valNotify = false;
+
+  onChangeFunction(bool newValue) {
+    setState(() {
+      valNotify = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,9 +90,6 @@ class Profile extends StatelessWidget {
         _buildSettingItem(context, 'My Account', 'Make changes to my profile'),
         const Divider(height: 0, indent: 20, endIndent: 20),
 
-        _buildSettingItem(context, 'Notifications', 'Adjust notificaton settings'),
-        const Divider(height: 0, indent: 20, endIndent: 20),
-
         _buildSettingItem(context, 'Help', 'Read FAQ'),
         const Divider(height: 0, indent: 20, endIndent: 20),
 
@@ -86,6 +97,9 @@ class Profile extends StatelessWidget {
         const Divider(height: 0, indent: 20, endIndent: 20),
 
         _buildSettingItem(context, 'Log Out', 'Leave the application'),
+        const Divider(height: 0, indent: 20, endIndent: 20),
+
+        _buildToggleItem(context, 'Dark Mode', 'Change between light/dark theme', valNotify, onChangeFunction),
         const Divider(height: 0, indent: 20, endIndent: 20),
       ],
     );
@@ -129,4 +143,43 @@ class Profile extends StatelessWidget {
     );
   }
 
+  GestureDetector _buildToggleItem(BuildContext context, String title, String subtitle, bool value, Function onChangeMethod) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title),
+                  const SizedBox(height: 8),
+                  Text(
+                      subtitle,
+                      style: const TextStyle(
+                          color: Colors.grey
+                      )),
+                ],
+              ),
+            ),
+            Transform.scale(
+              scale: 0.7,
+              child: CupertinoSwitch(
+                activeColor: Colors.blue,
+                value: value,
+                onChanged: (bool newValue) {
+                  onChangeMethod(newValue);
+                  print(newValue);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
