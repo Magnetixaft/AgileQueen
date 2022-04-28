bd06a8f9980556e0b37e113cf660031952167af8
magnetixaft <jsjsandberg@gmail.com>
2022-04-28 15:23:25 +0200

    Login working with Azure AD, connected to testing Org

--- lib/AuthenticationHandler.dart
+++ lib/AuthenticationHandler.dart
@@ -1,20 +1,24 @@
+import 'dart:convert';
+
 import 'package:aad_oauth/aad_oauth.dart';
 import 'package:aad_oauth/model/config.dart';
+import 'package:http/http.dart' as http;
 
 class AuthenticationHandler {
   static final AuthenticationHandler _instance = AuthenticationHandler._();
+  var accessToken;
 
   static final Config config = Config(
     tenant: '48306bc3-49ff-43e2-8964-4bd7d2dbba92',
     clientId: '8ba1b2e9-e630-4e49-909f-26101178f40d',
     scope: 'openid profile offline_access',
-    redirectUri: 'https://login.live.com/oauth20_desktop.srf',
+    redirectUri: 'msauth://com.example.flutter_application_1/Nwn3Du03NhXB4Wt4ZMuGlrxrLH8%3D',
   );
   final AadOAuth oauth = AadOAuth(config);
 
   AuthenticationHandler._();
 
-  static void initialize(){
+  static void initialize() {
   }
 
   static AuthenticationHandler getInstance() {
@@ -24,19 +28,16 @@ class AuthenticationHandler {
   void login() async {
     try {
       await oauth.login();
-      var accessToken = await oauth.getAccessToken();
+      accessToken = await oauth.getAccessToken();
       print('Logged in successfully, your access token: $accessToken');
-      } catch (e) {
+    } catch (e) {
       print(e);
     }
   }
+
   void logout() async {
     await oauth.logout();
     print('Logged out');
-  }
-
-  String dummyLogin() {
-    return"test@gmail.com";
   }
 
 }
