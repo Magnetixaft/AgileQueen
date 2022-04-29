88b2af3400b11e843b7964215823cf21d4e64a35
albertwickman <albert.wickman@hotmail.se>
2022-04-29 18:09:04 +0200

    Added AlertDialog to the listitems in profile_settings.dart

--- lib/tabs/profile_settings.dart
+++ lib/tabs/profile_settings.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  /*bool valNotify = false;
+
+  onChangeFunction(bool newValue) {
+    setState(() {
+      valNotify = newValue;
+    });
+  }
+  */
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +26,10 @@ class Profile extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         const SizedBox(height: 10),
-        const Text('    Profile',
+        const Text('    Menu',
           style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold
+              fontSize: 30,
+              fontWeight: FontWeight.bold
           ),
         ),
         const SizedBox(height: 20),
@@ -51,10 +66,10 @@ class Profile extends StatelessWidget {
                     Text(
                       'Albert Wickman', // TODO get name
                       style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        letterSpacing: 1
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          letterSpacing: 1
                       ),
                     ),
                     Text(
@@ -72,12 +87,8 @@ class Profile extends StatelessWidget {
         ),
 
 
-        /// Calls the builder method to build each item.
-        _buildSettingItem(context, 'My Account', 'Make changes to my profile'),
-        const Divider(height: 0, indent: 20, endIndent: 20),
 
-        _buildSettingItem(context, 'Notifications', 'Adjust notificaton settings'),
-        const Divider(height: 0, indent: 20, endIndent: 20),
+        /// Calls the builder method to build each item.
 
         _buildSettingItem(context, 'Help', 'Read FAQ'),
         const Divider(height: 0, indent: 20, endIndent: 20),
@@ -87,6 +98,8 @@ class Profile extends StatelessWidget {
 
         _buildSettingItem(context, 'Log Out', 'Leave the application'),
         const Divider(height: 0, indent: 20, endIndent: 20),
+
+
       ],
     );
 
@@ -98,6 +111,19 @@ class Profile extends StatelessWidget {
       behavior: HitTestBehavior.translucent,
       onTap: () {
         print('Item Tapped');
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text(title),
+                content: Text(subtitle),
+                actions: <Widget>[
+                  ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: Text('OK')),
+                ],
+              );
+            });
       },
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
@@ -114,8 +140,8 @@ class Profile extends StatelessWidget {
                   Text(
                       subtitle,
                       style: const TextStyle(
-                        color: Colors.grey
-                  )),
+                          color: Colors.grey
+                      )),
                 ],
               ),
             ),
@@ -129,4 +155,47 @@ class Profile extends StatelessWidget {
     );
   }
 
+
+
+/*
+  GestureDetector _buildToggleItem(BuildContext context, String title, String subtitle, bool value, Function onChangeMethod) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title),
+                  const SizedBox(height: 8),
+                  Text(
+                      subtitle,
+                      style: const TextStyle(
+                          color: Colors.grey
+                      )),
+                ],
+              ),
+            ),
+            Transform.scale(
+              scale: 0.7,
+              child: CupertinoSwitch(
+                activeColor: Colors.blue,
+                value: value,
+                onChanged: (bool newValue) {
+                  onChangeMethod(newValue);
+                  print(newValue);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+*/
 }
