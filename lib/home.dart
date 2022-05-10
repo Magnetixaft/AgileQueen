a6ca67aa53fc87ab8e1ae2ccd90ee1603fd79823
Andreas Helgesson <andreas.helgesson@hotmail.com>
2022-05-10 21:59:43 +0200

    Added documentation comments

--- lib/home.dart
+++ lib/home.dart
@@ -3,9 +3,10 @@ import 'package:flutter_application_1/tabs/booking_view.dart';
 import 'package:flutter_application_1/tabs/bookings.dart';
 import 'package:flutter_application_1/tabs/profile_settings.dart';
 
-/// A tab-based view that acts as the apps top most page
+/// A tab-based view that acts as the apps top most page.
 ///
 /// This page is navigated to when a user has logged in.
+/// The page has three tabs selecatble by the user: [BookingView], [Bookings] and [Profile].
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -15,16 +16,17 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   
-  /// The index currently selected in the BottomNavigationBar
+  /// The index currently selected in the BottomNavigationBar.
   int _selectedIndex = 0;
   
-  /// The list of tabs/pages selectable using the BottomNavigationBar
+  /// The list of tabs/pages selectable using the BottomNavigationBar.
   final List<Widget> _tabs = <Widget>[
     BookingView(),
     const Bookings(),
     const Profile()
   ];
-
+  
+  /// Switches tab to [index].
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
