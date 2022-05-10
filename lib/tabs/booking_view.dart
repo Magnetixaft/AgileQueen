0c640e6d4b24aa7b052a4112fdae91ad0ee92f23
Unchanging <1994.johan.andersson@gmail.com>
2022-05-10 20:47:38 +0200

    Added functions to FirebaseHandler to add and remove admins, divisions, and offices.

--- lib/tabs/booking_view.dart
+++ lib/tabs/booking_view.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/firebase_handler.dart';
